40b7ddf09becbe4b153cc1cd945f51969ce0d7e4
okrzyzos <58844026+okrzyzos@users.noreply.github.com>
2021-02-03 22:49:57 +0100

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,41 @@
+const btns = document.querySelectorAll('.btn-modale');
+const modaleEquipement = document.querySelector('.bloc-modale');
+const fermtureModale = document.querySelector('.fermeture');
+const imgIndex = document.querySelector('.bloc-modale img');
+
+
+
+
+if(window.matchMedia("(min-width:850px)").matches){
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
+            modaleEquipement.classList.add('active-modale');
+        })
+        })
+        fermtureModale.addEventListener('click', (e) => {
+            modaleEquipement.classList.remove('active-modale');
+        
+        
+        })
+        modaleEquipement.addEventListener('click', (e) => {
+            modaleEquipement.classList.remove('active-modale');
+        
+        
+        })
+
+
+
+
+}
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+if(window.scrollY > 30){
+    nav.classList.add('anim-nav');
+} else {
+    nav.classList.remove('anim-nav');
+
+}
+})
+
